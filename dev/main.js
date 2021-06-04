809de85c7c25a5883c144d80890e412c15c5d521
adrianpdm <adrian.padmodihardjo@gmail.com>
2021-06-04 23:14:38 +0700

    set /dev as vue-cli serve entry point

--- dev/main.js
+++ dev/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import App from '@/'
+import App from '@dev/App.vue'
 import router from './router'
 import store from './store'
 
--- dev/views/Home.vue
+++ dev/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    Test
   </div>
 </template>
 
