204176929b185a6e6a7ec972054667afd6a65286
adrianpdm <adrian.padmodihardjo@gmail.com>
2021-06-04 22:40:32 +0700

    fix build target (es5 and preset-env)

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,18 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ]
+  ],
+  env: {
+    es5: {
+      presets: ['@babel/preset-env'],
+    },
+    lib: {
+      presets: [
+        ['@babel/preset-env', {
+          targets: 'last 1 chrome version',
+          modules: false,
+        }],
+      ],
+    },
+  },
 }
--- scripts/build-lib.js
+++ scripts/build-lib.js
@@ -1,8 +1,9 @@
 'use strict'
 const shell = require('shelljs')
 
-shell.rm('-rf', 'lib', 'lib-temp')
+shell.rm('-rf', 'es5', 'lib', 'lib-temp')
 shell.exec('yarn run tsc -p tsconfig.build.json')
+shell.exec('yarn run cross-env NODE_ENV=es5 babel lib-temp --out-dir es5 --source-maps')
 shell.mv('./lib-temp/lib.js', './lib-temp/index.js')
 shell.exec('yarn run cross-env NODE_ENV=lib babel lib-temp --out-dir lib --source-maps')
 shell.rm('-rf', 'lib-temp')
